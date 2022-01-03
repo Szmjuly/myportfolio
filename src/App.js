c1e050f7082f558c6cb83385a076d00c3a9536b1
Solomon Markowitz <smarkowitz2018@fau.edu>
2022-01-02 22:52:39 -0500

    Added Home Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 //Components
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //Styling
-import './css/App.css';
+import './App.css';
+
+//Pages
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
           <Router basename={window.location.pathname || ''}>
               <Routes>
-                
+                <Route exact path="/" element={<HomePage />} />
               </Routes>
         </Router>
     );
